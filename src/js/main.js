4dd4acd048e63c8d1dcd73bd8f4871ee48b2ff21
ArturCiepielowski <artciepi@wp.pl>
2020-05-27 23:01:11 +0200

    feat:filled some blank spots

--- src/js/main.js
+++ src/js/main.js
@@ -59,45 +59,13 @@ navigationNc4.addEventListener("click", (e) => {
 
 //Page transition
 
-import Swup from 'swup';
-const swup = new Swup();
+
 
 
 
 // Buttons on experience page
 
 
-const slider = document.querySelector('.experience__sliderContainer');
-const leftArrow = document.querySelector('.left');
-const rightArrow = document.querySelector('.right');
-const indicatorParents = document.querySelector('.experience__controls ul')
-var sectionIndex = 0;
-
-document.querySelectorAll('.experience__controls li').forEach(function(indicator , ind){
-indicator.addEventListener('click' , function() {
-  sectionIndex=ind;
-  document.querySelector('.experience__controls .selected').classList.remove('selected');
-  indicator.classList.add('selected');
-  slider.style.transform= 'translate('+ (sectionIndex)* -33.333 +'%)';
-
-  });
-});
-
-leftArrow.addEventListener('click' , function(){
-  sectionIndex = (sectionIndex >0) ? sectionIndex - 1 : 0;
-  document.querySelector('.experience__controls .selected').classList.remove('selected');
-  indicatorParents.children[sectionIndex].classList.add('selected');
-  slider.style.transform= 'translate('+ (sectionIndex)* -33.333 +'%)';
-  
-  });
-
-rightArrow.addEventListener('click' , function(){
-sectionIndex = (sectionIndex <2) ? sectionIndex + 1 : 2;
-document.querySelector('.experience__controls .selected').classList.remove('selected');
-indicatorParents.children[sectionIndex].classList.add('selected');
-slider.style.transform= 'translate('+ (sectionIndex)* -33.333 +'%)';
-
-});
 
 
 
